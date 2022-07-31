bd00c42b0eafc62f3b73292827d33858435864f5
Pratham <pratham.webdev@gail.com>
2022-07-31 23:09:32 +0530

    port for heroku

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 // const express = require('express');
-const port = 3000;
 const app = express();
 
 import fetch from 'node-fetch';
@@ -22,6 +21,6 @@ app.get('/', (req, res)=>{
     
 });
 
-app.listen(`${port}`, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log('port is active');
 });
